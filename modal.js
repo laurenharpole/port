e41414face42ed061b5556a6fb722dc10ddcc84d
Lauren <lrh72@cornell.edu>
2020-06-28 03:14:59 -0400

    w

--- modal.js
+++ modal.js
@@ -16,17 +16,5 @@ var btn = document.getElementsByClassName("glow");
 btn[1].onclick = function() {
   modal[1].style.display = "visible";
 }
-// When the user clicks on <span> (x), close the modal
-// span[0].onclick = function() {
-//   modal[0].style.display = "none";
-// }
-//
-// span[1].onclick = function() {
-//   modal[1].style.display = "none";
-// }
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//       modal.style.display = "none";
-//   }
+
 }
